dea106b107727bb3276d8f269ae00bde077045d8
Olle <olleozon@gmail.com>
2022-04-01 21:57:46 +0200

    Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // ./src/App.tsx
-
 import React, { useState, useRef, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios'
+import { Button, InputGroup, Form } from 'react-bootstrap';
 import { useReactToPrint } from "react-to-print";
 import Path from 'path';
 import uploadFileToBlob, { isStorageConfigured, getBlobsList } from './azure-storage-blob';
@@ -10,12 +12,26 @@ const storageConfigured = isStorageConfigured();
 const App = (): JSX.Element => {
   // all blobs in container
   const [blobList, setBlobList] = useState<string[]>([]);
+  const [data, setData] = useState<any>(null);
+  const [addedData, setAddedData] = useState<any>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchList = async () => {
       setBlobList(await getBlobsList());
     }
-    fetchData();
+    fetchList();
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const jsonPath = urlParams.get('j');
+    const jsonKey = urlParams.get('k');
+    if (jsonPath && jsonPath.length > 0 && jsonKey && jsonKey.length > 0) {
+      axios.get(jsonPath + jsonKey)
+      .then(response => {
+          console.log(response.data);
+          setData(response.data);
+          if (response.data.MREC_Id) setAddedData({"MREC_Id": response.data.MREC_Id });
+      });
+    }
   }, []);
 
   // current file to upload into container
@@ -26,15 +42,23 @@ const App = (): JSX.Element => {
   const [inputKey, setInputKey] = useState(Math.random().toString(36));
 
   const onFileChange = (event: any) => {
-    // capture file into state
-    setFileSelected(event.target.files[0]);
+    setFileSelected(event.target.files[0]); // capture file into state
   };
 
-  const onFileUpload = async () => {
-    // prepare UI
-    setUploading(true);
+  const onSave = async (e: any) => {
+    e.preventDefault();
+    console.log(addedData);
+  }
 
-    // *** UPLOAD TO AZURE STORAGE ***
+  const onFormChange = (e: any) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+    setAddedData({ ...addedData, [name]: value });
+  }
+
+  const onFileUpload = async () => {
+    setUploading(true); // prepare UI
     const blobsInContainer: string[] = await uploadFileToBlob(fileSelected);
 
     // prepare UI for results
@@ -54,13 +78,35 @@ const App = (): JSX.Element => {
   // display form
   const DisplayForm = () => (
     <div>
-      <input type="file" onChange={onFileChange} key={inputKey || ''} />
-      <button type="submit" onClick={onFileUpload}>
-        Upload
-      </button>
-      <button type="button" onClick={onPrint} style={{  marginLeft: "10px" }}>
-        Print
-      </button>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Anmärkningstyp</Form.Label>
+          <Form.Control disabled value={data && data.REVI_Typ} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Anmärkning</Form.Label>
+          <Form.Control as="textarea" disabled value={data && data.REVI_Memo} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Korrigerande åtgärd</Form.Label>
+          <Form.Control as="textarea" name="RANM_Korrigering" value={data && data.RANM_Korrigering} onChange={onFormChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Signatur</InputGroup.Text>
+          <Form.Control as="input" name="RANM_Signatur" value={data && data.RANM_Signatur} onChange={onFormChange} />
+          <Button variant="outline-secondary" type="submit" onClick={onSave}>Spara</Button>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Bilaga</InputGroup.Text>
+            <Form.Control type="file" onChange={onFileChange} key={inputKey || ''} />
+            <Button variant="outline-secondary" onClick={onFileUpload}>Ladda upp</Button>
+            <Button variant="outline-secondary" onClick={onPrint}>Skriv ut</Button>
+          </InputGroup>
+        </Form.Group>
+      </Form>
     </div>
   )
   
@@ -85,8 +131,8 @@ const App = (): JSX.Element => {
   );
 
   return (
-    <div>
-      <h1>Upload file to Azure Blob Storage</h1>
+    <div style={{padding: "10px"}}>
+      <h1>Korrigering revisionsanmärkning {data && data.REVI_Id} </h1>
       {storageConfigured && !uploading && DisplayForm()}
       {storageConfigured && uploading && <div>Uploading</div>}
       <hr />
