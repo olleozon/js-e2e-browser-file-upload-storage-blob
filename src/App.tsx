fde4184b58321bcc4573bb9fdc47cb0e9b3872e6
Olle <olleozon@gmail.com>
2022-04-04 18:38:20 +0200

    Attachment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios'
-import { Button, InputGroup, Form } from 'react-bootstrap';
+import { Alert, Button, InputGroup, Form } from 'react-bootstrap';
 import { useReactToPrint } from "react-to-print";
 import Path from 'path';
 import uploadFileToBlob, { getBlobsList } from './azure-storage-blob';
@@ -12,48 +12,35 @@ const App = (): JSX.Element => {
   const [blobList, setBlobList] = useState<string[]>([]);
   const [data, setData] = useState<any>(null);
   const [addedData, setAddedData] = useState<any>(null);
-  const [params, setParams] = useState<{ [key: string]: string }>({});
+  const [attachment, setAttachment] = useState<string>("");
+  const [storageUrl, setStorageUrl] = useState<URL | null>(null); // JSON data blob storage url with sas token querystring
+  const [error, setError] = useState<string>("");
 
   const storageConfigured = (): boolean => {
-    return (params.hasOwnProperty("url") && params.hasOwnProperty("host") && params.hasOwnProperty("container") && params.hasOwnProperty("key"));
+    return (storageUrl != null);
   };
-
+  
   useEffect(() => {
-    const blobConfig =(params: string)=>{
-      const config: { [key: string]: string } = {};
-      if (params.length) {
-        const urlParams = new URLSearchParams(params);
-        const jPath = urlParams.get('j');
-        if (jPath != null) {
-          const urlPath = new URL(jPath);
-          if (urlPath != null) {
-            config.url = jPath;
-            config.host = urlPath.host;
-            if (urlPath.pathname.split('/').length > 1) config.container = urlPath.pathname.split('/')[1];
-          }
-        }
-        const jKey = urlParams.get('k');
-        if (jKey != null) {
-          config.key = jKey;
-        }
+    const jsonPath = new URLSearchParams(window.location.search).get('j');
+    if (jsonPath) {
+      const jsonUrl = new URL(jsonPath);
+      if (jsonUrl) {
+        setStorageUrl(jsonUrl);
+        axios.get(jsonPath)
+        .then(response => {
+            console.log(response.data);
+            setData(response.data);
+            if (response.data.MREC_Id) setAddedData({"MREC_Id": response.data.MREC_Id, "sas": jsonUrl.search });
+            if (response.data.INFO_PathFile) setAttachment(response.data.INFO_PathFile);
+            // Test: Show pictures in blob 
+            const fetchList = async () => {
+              setBlobList(await getBlobsList(jsonUrl));
+            }
+            fetchList();
+        })
+        .catch(error => setError(error.toString())
+        );
       }
-      setParams(config);
-      return config;
-    }
-
-    const p: { [key: string]: string } = blobConfig(window.location.search);
-    if (p.url && p.host && p.container && p.key) {
-      const fetchList = async () => {
-        setBlobList(await getBlobsList(p.host, p.container, p.key));
-      }
-      fetchList();
-
-      axios.get(p.url + p.key)
-      .then(response => {
-          console.log(response.data);
-          setData(response.data);
-          if (response.data.MREC_Id) setAddedData({"MREC_Id": response.data.MREC_Id });
-      });
     }
   }, []);
 
@@ -68,33 +55,42 @@ const App = (): JSX.Element => {
     setFileSelected(event.target.files[0]); // capture file into state
   };
 
+  const onShow = (e: any) => {
+    if (attachment.length > 0) window.open(attachment, "_blank");
+  }
+
   const onSave = async (e: any) => {
     e.preventDefault();
-    setAddedData({ "key": params.key });
-    setAddedData({ "MREC_Id": data.MREC_Id });
-    console.log(addedData);
-    // await axios.post(postTarget, addedData); // Needs CORS configuration
-    const aFile = new File([JSON.stringify(addedData)], 'arec-' + addedData.MREC_Id + '-answer.json', {type: 'application/json'});
-    await uploadFileToBlob(aFile, params.host, params.container, params.key);
-    const mFile = new File([JSON.stringify(data)], 'mrec-' + addedData.MREC_Id + '.json', {type: 'application/json'});
-    await uploadFileToBlob(mFile, params.host, params.container, params.key);
+    if (addedData && addedData.MREC_Id && addedData.sas) {
+      console.log(addedData);
+      // await axios.post(postTarget, addedData); // Needs CORS configuration
+      const aFile = new File([JSON.stringify(addedData)], 'arec-' + addedData.MREC_Id + '-answer.json', {type: 'application/json'});
+      await uploadFileToBlob(aFile, storageUrl);
+      const mFile = new File([JSON.stringify(data)], 'mrec-' + addedData.MREC_Id + '.json', {type: 'application/json'});
+      await uploadFileToBlob(mFile, storageUrl);
+    }
   }
 
   const onFormChange = (e: any) => {
     const name = e.target.name;
     const value = e.target.value;
     setData({ ...data, [name]: value });
-    setAddedData({ ...addedData, [name]: value });
+    setAddedData((addedData: any) =>({ ...addedData, ...{[name]: value} }));
   }
 
   const onFileUpload = async () => {
     if (storageConfigured()) {
       setUploading(true); // prepare UI
-      const blobsInContainer: string[] = await uploadFileToBlob(fileSelected, params.host, params.container, params.key);
+      const blobsInContainer: string[] = await uploadFileToBlob(fileSelected, storageUrl);
 
       // prepare UI for results
       setBlobList(blobsInContainer);
-
+      if (blobsInContainer.length > 0) {
+        const attachment = blobsInContainer[blobsInContainer.length - 1];
+        setAttachment(attachment);
+        setData({ ...data, "INFO_PathFile": attachment });
+        setAddedData((addedData: any) =>({ ...addedData, ...{"INFO_PathFile": attachment} }));
+      }
       // reset state/form
       setFileSelected(null);
       setUploading(false);
@@ -107,13 +103,18 @@ const App = (): JSX.Element => {
     content: () => componentRef.current,
   });
 
+  const isImageUrl = (url :string) :boolean => {
+    const urlNoQuery :string = (url.indexOf('?') > 0) ? url.substring(0, url.indexOf('?')) : url;
+    return (urlNoQuery.match(/\w+\.(jpg|jpeg|gif|png)$/gi) != null);
+  };
+
   // display form
   const DisplayForm = () => (
     <div>
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Anmärkningstyp</Form.Label>
-          <Form.Control disabled value={data && data.REVI_Typ} />
+          <Form.Control as="input" type="input" disabled value={data && data.REVI_Typ} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Anmärkning</Form.Label>
@@ -124,15 +125,27 @@ const App = (): JSX.Element => {
           <Form.Control as="textarea" name="RANM_Korrigering" value={data && data.RANM_Korrigering} onChange={onFormChange} />
         </Form.Group>
         <Form.Group className="mb-3">
-        <InputGroup className="mb-3">
-          <InputGroup.Text>Signatur</InputGroup.Text>
-          <Form.Control as="input" name="RANM_Signatur" value={data && data.RANM_Signatur} onChange={onFormChange} />
-          <Button variant="outline-secondary" type="submit" onClick={onSave}>Spara</Button>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Signatur</InputGroup.Text>
+            <Form.Control as="input" name="RANM_Signatur" value={data && data.RANM_Signatur} onChange={onFormChange} />
+            <Button variant="outline-secondary" type="submit" onClick={onSave}>Spara</Button>
           </InputGroup>
         </Form.Group>
         <Form.Group className="mb-3">
           <InputGroup className="mb-3">
-            <InputGroup.Text>Bilaga</InputGroup.Text>
+            <InputGroup.Text>Klart datum</InputGroup.Text>
+            <Form.Control type="date" name="RANM_KlartDatum" value={data && data.RANM_KlartDatum} onChange={onFormChange} />
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <InputGroup className="mb-3">
+          <InputGroup.Text>Bilaga</InputGroup.Text>
+            <Form.Control as="input" name="INFO_PathFile" value={attachment} onChange={onFormChange} />
+            <Button variant="outline-secondary" type="button" onClick={onShow}>Visa</Button>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <InputGroup className="mb-3">
             <Form.Control type="file" onChange={onFileChange} key={inputKey || ''} />
             <Button variant="outline-secondary" onClick={onFileUpload}>Ladda upp</Button>
             <Button variant="outline-secondary" onClick={onPrint}>Skriv ut</Button>
@@ -151,9 +164,10 @@ const App = (): JSX.Element => {
           return (
             <li key={item}>
               <div style={{ pageBreakInside: "avoid"}}>
-                {Path.basename(item)}
+                {new URL(item).pathname}
                 <br />
-                <img src={item} alt={item} style={{ maxWidth: "700px" }} />
+                {isImageUrl(item) && <img src={item} alt={item} style={{ maxWidth: "700px" }} />}
+                {!isImageUrl(item) && <a target='_blank' rel='noreferrer' href={item}>Dokument</a>}
               </div>
             </li>
           );
@@ -165,6 +179,7 @@ const App = (): JSX.Element => {
   return (
     <div style={{padding: "10px"}}>
       <h1>Korrigering revisionsanmärkning {data && data.REVI_Id} </h1>
+      {error.length > 0 && <Alert variant="danger">{error}</Alert>}
       {storageConfigured() && !uploading && DisplayForm()}
       {storageConfigured() && uploading && <div>Uploading</div>}
       <hr />
--- src/azure-storage-blob.ts
+++ src/azure-storage-blob.ts
@@ -4,16 +4,39 @@
 // THIS IS SAMPLE CODE ONLY - NOT MEANT FOR PRODUCTION USE
 import { BlobServiceClient, ContainerClient} from '@azure/storage-blob';
 
+const getBlobAccess = (blobUrl: URL | null): { [key: string]: string } => {
+  if (!blobUrl) return {};
+  const containerName: string = (blobUrl != null && blobUrl.pathname.split('/').length > 1)? blobUrl.pathname.split('/')[1] : '';
+  const blobAccess: { [key: string]: string } = { storageHost: blobUrl.hostname, containerName: containerName, sasToken: blobUrl.search };
+  return blobAccess;
+}
+
+const createContainerClient = async (blobUrl: URL | null) => {
+  const blobAccess = getBlobAccess(blobUrl);
+  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
+  const blobService = new BlobServiceClient(
+    `https://${blobAccess.storageHost}/${blobAccess.sasToken}`
+  );
+  const containerClient: ContainerClient = blobService.getContainerClient(blobAccess.containerName);
+  /*
+  await containerClient.createIfNotExists({
+    access: 'container',
+  });
+  */
+  return containerClient;
+}
+
 // <snippet_getBlobsInContainer>
 // return list of blobs in container to display
-const getBlobsInContainer = async (containerClient: ContainerClient, storageHost: string, containerName: string, sasToken: string) => {
+const getBlobsInContainer = async (containerClient: ContainerClient, blobUrl: URL | null) => {
   const returnedBlobUrls: string[] = [];
+  const blobAccess = getBlobAccess(blobUrl);
 
   // get list of blobs in container
   // eslint-disable-next-line
   for await (const blob of containerClient.listBlobsFlat()) {
     // if image is public, just construct URL
-    if (!(blob.name.toLowerCase().endsWith(".json"))) returnedBlobUrls.push(`https://${storageHost}/${containerName}/${blob.name}${sasToken}`);
+    if (!(blob.name.toLowerCase().endsWith('.json'))) returnedBlobUrls.push(`https://${blobAccess.storageHost}/${blobAccess.containerName}/${blob.name}${blobAccess.sasToken}`);
   }
 
   return returnedBlobUrls;
@@ -22,54 +45,25 @@ const getBlobsInContainer = async (containerClient: ContainerClient, storageHost
 
 // <snippet_createBlobInContainer>
 const createBlobInContainer = async (containerClient: ContainerClient, file: File) => {
-  
-  // create blobClient for container
   const blobClient = containerClient.getBlockBlobClient(file.name);
-
-  // set mimetype as determined from browser with file upload control
+  // Set mimetype as determined from browser with file upload control
   const options = { blobHTTPHeaders: { blobContentType: file.type } };
-
-  // upload file
-  await blobClient.uploadData(file, options);
+  await blobClient.uploadData(file, options); // upload file
 }
 // </snippet_createBlobInContainer>
 
-export const getBlobsList = async (storageHost: string, containerName: string, sasToken: string): Promise<string[]> => {
-  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
-  const blobService = new BlobServiceClient(
-    `https://${storageHost}/${sasToken}`
-  );
 
-  // get Container - full public read access
-  const containerClient: ContainerClient = blobService.getContainerClient(containerName);
-  // await containerClient.createIfNotExists({
-  //   access: 'container',
-  // });
-  return getBlobsInContainer(containerClient, storageHost, containerName, sasToken);
+export const getBlobsList = async (blobUrl: URL | null): Promise<string[]> => {
+  const containerClient = await createContainerClient(blobUrl);
+  return getBlobsInContainer(containerClient, blobUrl);
 }
 
 // <snippet_uploadFileToBlob>
-const uploadFileToBlob = async (file: File | null, storageHost: string, containerName: string, sasToken: string): Promise<string[]> => {
+const uploadFileToBlob = async (file: File | null, blobUrl: URL | null): Promise<string[]> => {
   if (!file) return [];
-
-  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
-  const blobService = new BlobServiceClient(
-    `https://${storageHost}/${sasToken}`
-  );
-
-  // get Container - full public read access
-  const containerClient: ContainerClient = blobService.getContainerClient(containerName);
-  /*
-  await containerClient.createIfNotExists({
-    access: 'container',
-  });
-  */
-
-  // upload file
-  await createBlobInContainer(containerClient, file);
-
-  // get list of blobs in container
-  return getBlobsInContainer(containerClient, storageHost, containerName, sasToken);
+  const containerClient = await createContainerClient(blobUrl);
+  await createBlobInContainer(containerClient, file); // upload file
+  return getBlobsInContainer(containerClient, blobUrl); // get list of blobs in container
 };
 // </snippet_uploadFileToBlob>
 
